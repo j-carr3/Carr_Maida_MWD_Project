01419715eb5e08aec4803fde45f1af98b5c722ff
j-carr3 <78447864+j-carr3@users.noreply.github.com>
2022-10-03 20:16:42 -0400

    Initial commit

---
+++ Services/Users.js
@@ -0,0 +1,39 @@
+const axios = window.axios;
+const url =
+  "https://my-json-server.typicode.com/kellybuchanan/WebDev-Spring2021";
+
+export const createUser = (id, firstName, lastName, email, password) => {
+  return axios({
+    method: "post",
+    url: `${url}/users`,
+    data: {
+      id,
+      firstName,
+      lastName,
+      email,
+      password
+    },
+    headers: {
+      "Content-Type": "application/json"
+    },
+    json: true
+  })
+    .then((response) => {
+      console.log("POST response: ", response);
+    })
+    .catch((err) => {
+      console.log("POST error: ", err);
+    });
+};
+
+export const getAllUsers = () => {
+  return axios
+    .get(`${url}/users`)
+    .then((response) => {
+      console.log(response.data);
+      return response.data;
+    })
+    .catch((err) => {
+      console.log("GET Error: ", err);
+    });
+};
