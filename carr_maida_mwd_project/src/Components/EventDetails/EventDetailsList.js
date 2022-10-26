3728d8e40321cf721178c1bff25f71056b2b0478
Taylor Maida <taymaida@Taylors-MacBook-Pro.local>
2022-10-26 19:10:58 -0400

    comments added

--- carr_maida_mwd_project/src/Components/EventDetails/EventDetailsList.js
+++ carr_maida_mwd_project/src/Components/EventDetails/EventDetailsList.js
@@ -1,1 +1,3 @@
 /* TODO: Add Parse stuff to get the list to show up here */
+
+//this file is something we will add to in the future, where each event can be clicked on and you can see a specific event's items and details
