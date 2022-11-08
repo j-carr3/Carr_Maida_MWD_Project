88288818cc63710163bb028c1341f7c84f14da88
Taylor Maida <taymaida@taylors-mbp.dhcp.nd.edu>
2022-11-08 11:21:32 -0500

    Protected Route Component added

---
+++ carr_maida_mwd_project/src/Components/ProtectedRoute/ProtectedRoute.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Navigate, useNavigate } from "react-router-dom"; 
+import { checkUser } from "../Auth/AuthService";
+
+const ProtectedRoute = {{ element: Component, ...rest }) => {
+	console.log("element: ", Component);
+	const navigate = useNavigate();
+	const goBackHandler = () => {
+		navigate("/auth");
+	};
+	if (checkUser()) {
+		return <Component />;
+	} else {
+		return (
+			<div>
+				<p>Unauthorized!</p> <button onClick={goBackHandler}>Go Back.</button>
+			</div>
+		);
+	}
+};
+
+export default ProtectedRoute;
