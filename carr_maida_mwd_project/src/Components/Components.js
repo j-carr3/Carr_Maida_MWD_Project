3e026cc4f360e91f774ebb87678530faff31fac7
Taylor Maida <taymaida@Taylors-MacBook-Pro.local>
2022-11-09 17:08:08 -0500

    comments added

--- carr_maida_mwd_project/src/Components/Components.js
+++ carr_maida_mwd_project/src/Components/Components.js
@@ -13,11 +13,14 @@ import {
   Routes,
 } from "react-router-dom";
 
+//Added a Protected Route for all pages beyond the auth page so the user can only get to these pages if they are registered and logged in
+
+//It is important that we added this Protected Route to all of the different pages because even though the user automatticaly starts on the authorization page, they make be able to add something to the link to try to get to a different page even though they are not registered
 export default function Components() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ProtectedRoute path="/" element={Home} />} />
+        <Route path="/" element={<ProtectedRoute path="/" element={Home} />} /> 
 		<Route path="/auth" element={<AuthModule />} />
 		<Route path="/auth/register" element={<AuthRegister />} />
 		<Route path="/auth/login" element={<AuthLogin />} />
@@ -29,3 +32,6 @@ export default function Components() {
     </Router>
   );
 }
+
+
+// in the future we could add a protected route to certain event pages based on if you are invited to them or not so that users cannot access event pages to the events they are not apart of
--- carr_maida_mwd_project/src/Components/ProtectedRoute/ProtectedRoute.js
+++ carr_maida_mwd_project/src/Components/ProtectedRoute/ProtectedRoute.js
@@ -2,15 +2,20 @@ import React from "react";
 import { useNavigate } from "react-router-dom"; 
 import { checkUser } from "../Auth/AuthService";
 
+
+// This is the ProtectedRoute Component that is necessary to ensure unregisterd users cannot access our site
+
 const ProtectedRoute = ({ element: Component, ...rest }) => {
 	console.log("element: ", Component);
 	const navigate = useNavigate();
 	const goBackHandler = () => {
 		navigate("/auth");
 	};
+	//checkUser will return the component only if the user is registered and logged in
 	if (checkUser()) {
 		return <Component />;
 	} else {
+	//Otherwise, the user will receive a message telling them that they are unauthorized and can click on a button taking them back to the authorization page
 		return (
 			<div>
 				<p>Unauthorized!</p> <button onClick={goBackHandler}>Go Back.</button>
