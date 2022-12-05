1a3522cb1a8050129df1281692000c93306d6299
Taylor Maida <taymaida@taylors-mbp.dhcp.nd.edu>
2022-12-05 17:24:46 -0500

    Spotify Auth

--- carr_maida_mwd_project/src/Components/Home/Home.js
+++ carr_maida_mwd_project/src/Components/Home/Home.js
@@ -1,4 +1,6 @@
 import Header from "../Header/Header.js"
+import SpotifyAuth from "../Spotify/Auth.js"
+
 
 export default function Home() {
     return (
@@ -6,6 +8,8 @@ export default function Home() {
         <Header />
         <h1>Welcome to the Home component</h1>
         <p>This is the home component</p>
+        <p>To use the full functionality of our app, please login to Spotify</p>
+        <SpotifyAuth />
       </section>
     );
 }
