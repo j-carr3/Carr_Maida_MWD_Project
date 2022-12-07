7e4624422ef2484708ff6008c367199ff8454117
Taylor Maida <taymaida@taylors-mbp.dhcp.nd.edu>
2022-12-06 20:45:39 -0500

    weird error fixed

--- carr_maida_mwd_project/src/Components/Spotify/Search.js
+++ carr_maida_mwd_project/src/Components/Spotify/Search.js
@@ -25,23 +25,23 @@ function SpotifySearch() {
 
     const searchSongs = async (e) => {
         e.preventDefault()
-        const {data} = await axios.get("http://https://api.spotify.com/v1/search", {
+        const {data} = await axios.get("https://api.spotify.com/v1/search", {
             headers: {
                 Authorization: `Bearer ${token}`
             },
-        params: {
-            q: searchKey,
-            type: "track"
-        }
-    })
+            params: {
+                q: searchKey,
+                type: "track"
+            }
+        })
 
-    setSongs(data.songs.items)
-}
+        setSongs(data.songs.items)
+    }
 
-<form onSubmit={searchSongs}>
-    <input type="text" onChange={e => setSearchKey(e.target.value)}/>
-    <button type={"submit"}>Search</button>
-</form>
+//<form onSubmit={searchSongs}>
+//    <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+ //   <button type={"submit"}>Search</button>
+//</form>
 
 const renderSongs = () => {
     return songs.map(song => (
