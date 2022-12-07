12e698e7ec575031fc048601d8fdf3f85dc6d12f
j-carr3 <lilcarr35@gmail.com>
2022-12-07 11:33:56 -0500

    itermediate commit

--- carr_maida_mwd_project/src/Services/ItemsService.js
+++ carr_maida_mwd_project/src/Services/ItemsService.js
@@ -1,9 +1,10 @@
-// this service is for reading all items from the datbase using Parse
-
-
 import Parse from "parse";
+import { getAllEvents } from "../Components/CreateEvent/EventService";
+//import { getById } from "../Components/CreateEvent/EventService.js";
+/* SERVICE FOR PARSE SERVER OPERATIONS */
 
-export default function getItems() {
+// READ OPERATION - Read all items
+export const getItems = () => {
 	const Item = Parse.Object.extend("items");
 	const query = new Parse.Query(Item);
 
@@ -11,3 +12,40 @@ export default function getItems() {
 		return result;
 	});
 }
+
+// READ OPERATION - Read item based on ID
+export const getItemById = (id) => {
+	const Item = Parse.Object.extend("items");
+	const query = new Parse.Query(Item);
+
+	return query.find(id).then((result) => {
+		return result;
+	});
+}
+
+export const createItem = (newItem) => {
+	const Item = Parse.Object.extend("items");
+	const item = new Item();
+
+	console.log(newItem.event_id);
+	getAllEvents().then((result) => {
+		const resultEvent = result.find((resultItem) => resultItem.id === newItem.event_id);
+		console.log(resultEvent);
+	});
+	
+	item.set("submitted_by", Parse.User.current());
+	item.set("item_name", newItem.item_name);
+	item.set("price_per_unit", parseInt(newItem.price_per_unit));
+	item.set("quantity", parseInt(newItem.quantity));
+	item.set("event_id", newItem.event_id);
+
+  return item.save()
+  .then((itemResult) => {
+    // Execute any logic that should take place after the object is saved.
+    alert('New item created with objectId: ' + itemResult.id);
+  }, (error) => {
+    // Execute any logic that should take place if the save fails.
+    // error is a Parse.Error with an error code and message.
+    alert('Failed to create new object, with error code: ' + error.message);
+  });
+};
