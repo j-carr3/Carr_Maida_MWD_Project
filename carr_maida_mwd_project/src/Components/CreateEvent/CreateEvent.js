797e539fa93965fcfbc911bc62cbdb994a8117b2
Taylor Maida <taymaida@dhcp-10-7-170-0.dhcp.nd.edu>
2022-12-07 18:07:17 -0500

    original navbar added

--- carr_maida_mwd_project/src/Components/CreateEvent/CreateEvent.js
+++ carr_maida_mwd_project/src/Components/CreateEvent/CreateEvent.js
@@ -1,13 +1,16 @@
 import React from "react";
 //import EventCreation from "./NewEvent"
-import EventCreation from "./NewEvent.js"
-import Header from "../Header/Header.js"
+import EventCreation from "./NewEvent.js";
+//import Header from "../Header/Header.js"
+//import ResponsiveAppBar from "../Header/MuiHeader2.js"
+import NavBar from "../Header/MuiHeader2.js";
+
 
 /* MAIN MODULE WITH STATEFUL PARENT AND STATELESS CHILD */
 const EventModule = () => {
   return (
     <div>
-      <Header />
+      <NavBar />
       <h2>Create a new event! </h2>
       <EventCreation/>
     </div>
---
+++ carr_maida_mwd_project/src/Components/Header/MuiHeader2.js
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/system';
+import { Link } from "react-router-dom";
+
+const NavLinkStyled = styled('div')({
+  marginRight: '10px',
+  display: 'flex',
+});
+
+const LinkStyled = styled(Link)({
+    textDecoration: 'none',
+    color: 'white',
+    fontSize: '20px',
+    marginRight: '10px',
+    "&:hover": {
+      color: 'yellow',
+      borderBottom: "1px solid white",
+    },
+});
+
+
+function NavBar () {
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <AppBar position="static" sx={{ bgcolor: '#b6c199'}}>
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Web Dev Event Planner
+          </Typography>
+          <NavLinkStyled>
+            <LinkStyled to="/"> 
+              Home
+            </LinkStyled>
+            <LinkStyled to="/Events">
+              Events
+            </LinkStyled>
+          </NavLinkStyled>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default NavBar;
