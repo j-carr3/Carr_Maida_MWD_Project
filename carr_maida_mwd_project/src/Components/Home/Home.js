eed3f854e4e5e31d7e8bbb29a1439bcd709a1509
Taylor Maida <taymaida@dhcp-10-7-170-0.dhcp.nd.edu>
2022-12-10 16:37:36 -0500

    made addsongs a function whoops

--- carr_maida_mwd_project/src/Components/Home/Home.js
+++ carr_maida_mwd_project/src/Components/Home/Home.js
@@ -1,13 +1,13 @@
-import Header from "../Header/Header.js"
+import NavBar from "../Header/MuiHeaderHome.js"
 import SpotifyAuth from "../Spotify/Auth.js"
 
 
 export default function Home() {
     return (
       <section>
-        <Header />
-        <h1>Welcome to the Home component</h1>
-        <p>This is the home component</p>
+        <NavBar />
+        <h1>Welcome to the EventMate Home Page</h1>
+        <p>Navigate to your desired page.</p>
         <SpotifyAuth />
       </section>
     );
--- carr_maida_mwd_project/src/Components/Spotify/AddSongs.js
+++ carr_maida_mwd_project/src/Components/Spotify/AddSongs.js
@@ -2,69 +2,100 @@ import {useState, useEffect} from 'react';
 import axios from 'axios';
 import { useParams } from "react-router-dom";
 
-const { eventId } = useParams();
-const [playlist, setPlaylist] = useState("")
-const [track, setTrack] = useState({
-    track_name: "",
-    track_artist: ""
-});
+function AddSong () {
+    const { eventId } = useParams();
+    const [playlist, setPlaylist] = useState("")
+    const [track, setTrack] = useState({
+        track_name: "",
+        track_artist: "",
+        track_uri: ""
+    });
 
-useEffect(() => {
-    const hash = window.location.hash
-    let token = window.localStorage.getItem("token")
+    useEffect(() => {
+        const hash = window.location.hash
+        let token = window.localStorage.getItem("token")
 
-    if (!token && hash) {
-        token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+        if (!token && hash) {
+            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
 
-        window.location.hash = ""
-        window.localStorage.setItem("token", token)
+            window.location.hash = ""
+            window.localStorage.setItem("token", token)
+        }
+
+        setToken(token)
+
+    }, [])
+
+    useEffect(() => {
+        if (load) {
+        getAllPlaylists().then((result) => {
+            let resultPlaylist = {}
+            resultPlaylist = result.find((resultPlaylist) => resultPlaylist.playlist_event === eventId);
+
+            setPlaylist(resultPlaylist.playlist_id);
+
+            setLoad(false);
+
+        }, (error) => {
+            // Execute any logic that should take place if the save fails.
+            // error is a Parse.Error with an error code and message.
+            alert('Error: ' + error.message);
+        });
+        }
+    }, [playlist, eventId, load]);
+
+    const searchTracks = async (song_title, song_artist) => {
+        e.preventDefault()
+        const {data} = await axios.get("https://api.spotify.com/v1/search", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            params: {
+                q: song_title,
+                type: "track",
+                limit: 20
+            }
+        })
+
+        for (var item in data.tracks) {
+            if (item.album.artists.name === song_artist) {
+                setTrack({
+                    ...track,
+                    track_name: song_title, 
+                    track_artist: song_artist,
+                    track_uri: item.uri
+                });
+                break;
+            }
+        }
+
+        addSong(track);
+
     }
 
-    setToken(token)
-
-}, [])
-
-useEffect(() => {
-    if (load) {
-    getAllPlaylists().then((result) => {
-        let resultPlaylist = {}
-        resultPlaylist = result.find((resultPlaylist) => resultPlaylist.playlist_event === eventId);
-
-        setPlaylist(resultPlaylist.playlist_id);
-
-        setLoad(false);
-
-    }, (error) => {
-        // Execute any logic that should take place if the save fails.
-        // error is a Parse.Error with an error code and message.
-        alert('Error: ' + error.message);
-      });
+    const addSong = async(track) => {
+        try  {
+            const headersCust = {
+                Authorization: `Bearer ${token}`
+            }
+            const params = {
+                playlist_id: playlist,
+                uris: track.track_uri
+            }
+            e.preventDefault();
+            const {data} =  await axios.post(`	https://api.spotify.com/v1/playlists/${playlist_id}/tracks`,
+                params,{
+                headers: headersCust
+            })
     }
-}, [playlist, eventId, load]);
 
-const searchTracks = async (song_title, song_artist) => {
-    e.preventDefault()
-    const {data} = await axios.get("https://api.spotify.com/v1/search", {
-        headers: {
-            Authorization: `Bearer ${token}`
-        },
-        params: {
-            q: song_title,
-            type: "track",
-            limit: 20
+        catch {
+            alert('Error: Could not find song' + error.message);
         }
-    })
 
-    for (var item in data.tracks) {
-        if (item.album.artists.name === song_artist) {
-            setTrack({
-                ...track,
-                track_name: song_title, 
-                track_artist: song_artist
-            });
-            break;
-        }
     }
 
 }
+
+export default AddSong;
 
